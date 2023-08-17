5fc3415243a07456a49621cdd3f97671ce675041
abrorm3 <abrormukhammadiev@gmail.com>
2023-08-18 03:33:15 +0500

    created/styled template. Added router

---
+++ tagChatFront/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainChatComponent } from './main-chat/main-chat.component';
+
+const routes: Routes = [{ path: '', component: MainChatComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- tagChatFront/src/app/app.module.ts
+++ tagChatFront/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainChatComponent } from './main-chat/main-chat.component';
+import { TagPanelComponent } from './main-chat/tag-panel/tag-panel.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MainChatComponent, TagPanelComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
